b041d701467d159731b7c7f12534f08f79bce30e
F2V38Q6 <richard.tilt@fiserv.com>
2024-05-16 17:47:11 -0400

    COM-3473 added archives

--- package/Package.swift
+++ package/Package.swift
@@ -13,6 +13,6 @@ let package = Package(
     targets: [
         .binaryTarget(name: "FiservTTP",
                       url: "https://gitlab.onefiserv.net/na/gbs/carat-digital-ecommerce/commercehub/sdk/ch-apple-ttp/-/raw/main/FiservTTP.xcframework.zip",
-                      checksum: "1f9a08d19372fdc5f79d5f2c0c4f3090e6c99d17f6bb955248f52109f47bd04f")
+                      checksum: "6340962aab92b31015f67ed448693229c647f911397282fd0f905c9885ff1c8b")
     ]
 )
