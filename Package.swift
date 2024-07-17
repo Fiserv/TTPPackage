26e345979f954e328eb7951d5b087f341b698883
fs-rich-tilt <richard.tilt@fiserv.com>
2024-07-17 10:13:06 -0400

    package platform value

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "FiservTTP",
     platforms: [
-            .iOS("16.4")
+            .iOS("16.7")
         ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
